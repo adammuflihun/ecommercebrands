918c420c7c36a350fba193e55dfca2d81f2f0f21
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-09-09 09:30:31 +0700

    s

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
 import Swiper from 'swiper';
 
 window.Webflow ||= [];
-window.Webflow.push(() => {});
+window.Webflow.push(() => {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+});
